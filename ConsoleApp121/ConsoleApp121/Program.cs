26ded1803e78e0de71453d46aca80b4987359d68
AugustoVitoriano <112523031+AugustoVitoriano@users.noreply.github.com>
2024-05-13 21:18:09 -0300

    Add files via upload

---
+++ ConsoleApp121/ConsoleApp121/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp121
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double altura;
+
+            Console.Write("Informe o numero de pessoas: ");
+            n = int.Parse(Console.ReadLine());
+
+
+
+            double[] vect = new double[n];
+
+
+
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.Write("Altura {0}: ",i);
+                vect[i] = double.Parse(Console.ReadLine());
+            }
+
+
+
+
+
+
+        }
+    }
+}
