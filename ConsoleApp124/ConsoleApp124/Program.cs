9c28799b7fad98fd45ddbebeaba07897c6fd2581
AugustoVitoriano <112523031+AugustoVitoriano@users.noreply.github.com>
2024-05-13 21:19:32 -0300

    Repositório teste
    C#

---
+++ ConsoleApp124/ConsoleApp124/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp124
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double N1, N2, N3, C;
+
+            Console.Write("Informe o 1º numero: ");
+            N1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2º numero: ");
+            N2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 3º numero: ");
+            N3 = double.Parse(Console.ReadLine());
+
+            if (!(N3 > 5))
+            {
+                C = (N1 + N2) * N3;
+                Console.WriteLine("Resultado: {0}", C);
+            }
+            else 
+            {
+                C = (N1 - N2) * N3;
+                Console.WriteLine("Resultado: {0}", C);
+            }
+        }
+    }
+}
